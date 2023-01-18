db513403e2a8464a60d0f4135b5d21a32727cf98
Marcelo Llanos Maldonado <mrcl562@gmail.com>
2023-01-17 23:55:52 -0400

    fixed function to add menu items and get the total

--- restaurant.js
+++ restaurant.js
@@ -1,13 +1,14 @@
 const menu_items = document.querySelector(".order_wrapper");
 const order_items = document.querySelector(".order_items");
 var menu_item = document.querySelectorAll(".menu_item");
+var total = 0;
 const menu_keys ={
     burger: [5.99, "Classic Burger", 0],
     bbq: [10.59, "BBQ Burger", 0],
     chicken: [10.59, "Chicken Sandwich", 0],
     veggie: [7.59, "Veggie Burger", 0],
     fries: [1.99, "Fries", 0],
-    milkshake: [2.59, "milkshake", 0],
+    milkshake: [2.59, "Milkshake", 0],
 };
 
 //listener to add items to order
@@ -18,7 +19,6 @@ menu_item.forEach(item => {
         }
     })
 })
-
 
 const item_quantitys = (click_item) => {
   const menuMap = {
@@ -34,9 +34,11 @@ const item_quantitys = (click_item) => {
     menu_keys[menuKey][2]++;
     if(menu_keys[menuKey][2] == 1){
       add_order_item(menu_keys[menuKey] , `${menuKey}_quantity`, `${menuKey}_price`)
+      update_total();
     }else{
       document.querySelector(`.${menuKey}_quantity`).innerText = menu_keys[menuKey][2];
-      document.querySelector(`.${menuKey}_price`).innerText = menu_keys[menuKey][0] * menu_keys[menuKey][2];
+      document.querySelector(`.${menuKey}_price`).innerText = (menu_keys[menuKey][0] * menu_keys[menuKey][2]).toFixed(2); 
+      update_total();
     }
   } else {
     return false;
@@ -53,4 +55,11 @@ const add_order_item = (item, unique_class, unique_price) =>{
             )              
 }
 
+//total to pay function
+function update_total(){ 
+  let all_item_prices = Array.from(document.querySelectorAll(".item_price")).map(ele => +ele.innerText);
+  let total_order_price = document.querySelector(".total_price");
+  total = all_item_prices.reduce((a, b) => a + b ,0);
+  total_order_price.innerText =`${total.toFixed(2)} $`;
+}
 
